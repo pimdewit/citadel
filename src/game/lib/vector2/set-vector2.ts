a639c9920aedba4cfaa1d99f32e03753ee77a8ff
Pim de Wit <pim@pdw.io>
2022-09-08 17:54:27 +0100

    feat(Controls): added keyboard input, added transform interpolation

--- src/game/lib/vector2/set-vector2.ts
+++
@@ -1,8 +0,0 @@
-import {Vector2} from './typings';
-
-export function setVector2(arr: Vector2, x: number, y: number) {
-  arr[0] = x;
-  arr[1] = y;
-
-  return arr;
-}
--- src/game/lib/vector3/set-vector3.ts
+++
@@ -1,9 +0,0 @@
-import {Vector3} from './typings';
-
-export function setVector3(vector: Vector3, x: number, y: number, z: number) {
-  vector[0] = x;
-  vector[1] = y;
-  vector[2] = z;
-
-  return vector;
-}
