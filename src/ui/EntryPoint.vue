ea11b9d2a9f54796f4c8412287e0f63377124bea
Pim de Wit <pim@pdw.io>
2022-09-19 12:15:06 +0100

    feat(Logo): add document outline

--- src/ui/EntryPoint.vue
+++ src/ui/EntryPoint.vue
@@ -1,11 +1,37 @@
 <template>
-  <GameShell/>
+  <div class="game-wrapper">
+    <GameShell/>
+    <div class="game-hud">
+      <GameHud/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import GameHud from "./GameHud.vue";
 import GameShell from "./GameShell.vue";
 
 export default {
-  components: {GameShell}
+  components: {GameHud, GameShell}
 }
 </script>
+
+<style lang="scss" scoped>
+.game-wrapper {
+  display: grid;
+  height: 100%;
+  grid-template-areas: 'cell';
+
+  > * {
+    grid-area: cell;
+  }
+}
+
+.game-hud {
+  pointer-events: none;
+
+  > * {
+    pointer-events: auto;
+  }
+}
+</style>
---
+++ src/ui/GameHud.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Citadel</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 0.5rem;
+  padding: 1rem;
+  text-transform: uppercase;
+  -webkit-font-smoothing: none;
+}
+</style>
