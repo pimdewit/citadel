affbc1fc533a34ea891a81eba3a968b6bfeabc76
Pim de Wit <pim@pdw.io>
2022-09-25 15:47:46 +0100

    hotfix(Reveal): allow reveal from multiple sources

--- src/game/ecs/systems/vision-system.ts
+++ src/game/ecs/systems/vision-system.ts
@@ -5,8 +5,6 @@ import {
   hasComponent,
   removeComponent,
 } from 'bitecs';
-import {Mesh} from 'three';
-import {ColorIdentifier} from '../../_resources/colors';
 import {distanceToComponent} from '../../lib/math/vector3/distance-to';
 import {World} from '../../types';
 import {Position} from '../components/position';
@@ -19,12 +17,14 @@ export function visionSystem() {
   const enemyQuery = defineQuery([Position, Perceivable]);
 
   return defineSystem((world: World) => {
-    const entities = entityQuery(world);
+    const allies = entityQuery(world);
     const enemies = enemyQuery(world);
 
-    for (let i = 0; i < entities.length; ++i) {
-      const entity = entities[i];
-      const radius = Vision.radius[entity];
+    const revealedEnemies: Set<number> = new Set();
+
+    for (let i = 0; i < allies.length; ++i) {
+      const ally = allies[i];
+      const radius = Vision.radius[ally];
 
       // Vision has no radius, continue.
       if (!radius) continue;
@@ -35,21 +35,12 @@ export function visionSystem() {
       for (let j = 0; j < enemies.length; j++) {
         const enemy = enemies[j];
 
-        const distance = distanceToComponent(Position, entity, Position, enemy);
+        const distance = distanceToComponent(Position, ally, Position, enemy);
         const isWithinDistance = distance >= 0 && distance < radius;
 
-        isWithinDistance
-          ? addComponent(world, Revealed, enemy)
-          : removeComponent(world, Revealed, enemy);
+        if (!isWithinDistance) continue;
 
-        if (!isWithinDistance) {
-          // if (hasComponent(world, Revealed, enemy)) {
-          //   removeComponent(world, Revealed, enemy);
-          // }
-          continue;
-        }
-
-        // addComponent(world, Revealed, enemy);
+        revealedEnemies.add(enemy);
 
         if (distance < closestDistance) {
           closestDistance = distance;
@@ -57,7 +48,19 @@ export function visionSystem() {
         }
       }
 
-      Vision.target[entity] = closestEntity;
+      Vision.target[ally] = closestEntity;
+    }
+
+    // Set revealed state.
+    for (let i = 0; i < enemies.length; i++) {
+      const enemy = enemies[i];
+      const isRevealed = hasComponent(world, Revealed, enemy);
+
+      if (revealedEnemies.has(enemy)) {
+        if (!isRevealed) addComponent(world, Revealed, enemy);
+      } else if (isRevealed) {
+        removeComponent(world, Revealed, enemy);
+      }
     }
 
     return world;
